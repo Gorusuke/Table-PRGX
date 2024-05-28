b681fd814caae67833639b9f6e782ddac5717b6f
Jose Andres Garcia <Gorusuke123@gmail.com>
2024-05-28 16:42:48 -0500

    Add some icons (delete, edit and search)

---
+++ src/icons/AllIcons.jsx
@@ -0,0 +1,53 @@
+export function TrashICon () {
+  return (
+    <svg width="24px" height="24px" viewBox="0 0 24 24" strokeWidth="1.5" fill="none" xmlns="http://www.w3.org/2000/svg"  color="#000000">
+      <path d="M20 9L18.005 20.3463C17.8369 21.3026 17.0062 22 16.0353 22H7.96474C6.99379 22 6.1631 21.3026 5.99496 20.3463L4 9" 
+        stroke="#ff0000"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round">
+      </path>
+      <path
+        d="M21 6L15.375 6M3 6L8.625 6M8.625 6V4C8.625 2.89543 9.52043 2 10.625 2H13.375C14.4796 2 15.375 2.89543 15.375 4V6M8.625 6L15.375 6"
+        stroke="#ff0000"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round">
+      </path>
+    </svg>
+  )
+}
+
+export function EditICon () {
+  return (
+    <svg width="24px" height="24px" viewBox="0 0 24 24"strokeWidth="1.5"fill="none"xmlns="http://www.w3.org/2000/svg"color="#000000">
+      <path
+        d="M14.3632 5.65156L15.8431 4.17157C16.6242 3.39052 17.8905 3.39052 18.6716 4.17157L20.0858 5.58579C20.8668 6.36683 20.8668 7.63316 20.0858 8.41421L18.6058 9.8942M14.3632 5.65156L4.74749 15.2672C4.41542 15.5993 4.21079 16.0376 4.16947 16.5054L3.92738 19.2459C3.87261 19.8659 4.39148 20.3848 5.0115 20.33L7.75191 20.0879C8.21972 20.0466 8.65806 19.8419 8.99013 19.5099L18.6058 9.8942M14.3632 5.65156L18.6058 9.8942"
+        stroke="#000000"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round">
+      </path>
+    </svg>
+  )
+}
+
+export function SearchICon () {
+  return (
+    <svg width="24px" height="24px" viewBox="0 0 24 24" strokeWidth="1.5" fill="#fff0" xmlns="http://www.w3.org/2000/svg" color="#fff">
+      <path d="M17 17L21 21"
+        stroke="#fff"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round">
+      </path>
+    <path 
+      d="M3 11C3 15.4183 6.58172 19 11 19C13.213 19 15.2161 18.1015 16.6644 16.6493C18.1077 15.2022 19 13.2053 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11Z" 
+      stroke="#fff"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round">
+    </path>
+  </svg>
+  )
+}
