87a9888c7a4c22cc457c1a1ea1cf86bdefec040f
Jose Andres Garcia <Gorusuke123@gmail.com>
2024-05-28 16:44:59 -0500

    Add function to update and delete rows

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -6,11 +6,13 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { Button, Stack, TablePagination } from '@mui/material';
+import { IconButton, Stack, TablePagination } from '@mui/material';
 import { DataContext } from '../context/DataContext.jsx';
+import { EditICon, TrashICon } from '../icons/AllIcons.jsx';
+import { URL } from '../constants.js';
 
 const DataTable = () => {
-  const { data } = useContext(DataContext)
+  const { data, setAddNewData, setEditData} = useContext(DataContext)
   const [rowsPerPage, setRowsPerPage] = useState(15);
   const [page, setPage] = useState(0);
 
@@ -23,10 +25,29 @@ const DataTable = () => {
     setPage(0);
   };
 
+  const handleDeleteRow = async (id) => {
+    // best option
+    await fetch(`${URL}/posts/${id}`, { method: 'DELETE' });
+
+    // option 2
+    const filteredData = data.filter(row => row.id === id) // <- poner el !
+    // update localStorage
+    console.log(filteredData)
+  }
+
+  const handleEditRow = (row) => {
+    setAddNewData(true)
+    setEditData({
+      title: row.title,
+      description: row.body,
+      id: row.id
+    })
+  }
+
   const dataPerPage = data.slice(page * rowsPerPage, rowsPerPage * (page + 1))
 
   return (
-    <Paper sx={{ width: '100%', mb: 2 }}>
+    <Paper sx={{ width: '1000px' }}>
       <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} size='small' aria-label="simple table">
         <TableHead>
@@ -47,9 +68,13 @@ const DataTable = () => {
               <TableCell align="left">{row.title}</TableCell>
               <TableCell align="center">{row.body.slice(0, 20)}</TableCell>
               <TableCell align="center">
-                <Stack direction='row' spacing={2}>
-                  <Button onClick={() => {}} size='small' color='secondary' variant='outlined'>Edit</Button>
-                  <Button onClick={() => {}} size='small' color='error' variant='outlined'>Eliminate</Button>
+                <Stack direction='row' spacing={1}>
+                  <IconButton onClick={() => handleEditRow(row)} size='small' color='secondary' aria-label='update row'>
+                    <EditICon />
+                  </IconButton>
+                  <IconButton onClick={() => handleDeleteRow(row.id)} size='small' color='error' aria-label='eliminate row'>
+                    <TrashICon />
+                  </IconButton>
                 </Stack>
               </TableCell>
             </TableRow>
